c35dc6f5c9a4d0b4cba56cf9013b5ee05fda5f30
Yasmine-Hmida <hmidayasmiine@gmail.com>
2025-07-18 13:15:49 +0100

    Styling and designing the Login and SignUp pages

--- HabbitFlow/src/LoginFormComponent/LoginForm.jsx
+++ HabbitFlow/src/LoginFormComponent/LoginForm.jsx
@@ -1,14 +1,42 @@
 import { useNavigate } from 'react-router-dom'
 
+import view from '../assets/view.png'
+// import hide from '../assets/hide.png'
+
+import '../Pages/Register.css'
+
 const LoginForm = () => {
     const navigate = useNavigate();
     return (
         <>
             <button className='goHomeBtn' onClick={() => navigate('/')}>Go Back to Homepage</button>
-        
-            <div className='loginDiv'>
+            <form className='registerForm'>
+                <h1 className="registerTitle">Login</h1>
+
+                <div className="usernameContainer">
+                    <input type="text" name="username" className='registerInput' id="username" placeholder='Username'/>
+                    <p className="userNameMessage"></p>
+                </div>
                 
-            </div>
+                <div className="emailContainer">
+                    <input type="text" name="email" className='registerInput' id="email" placeholder='Email'/>
+                    <p className="emailMessage"></p>
+                </div>
+
+                <div className="passwordContainer">
+                    <input type="password" name="password" className='registerInput' id="password" placeholder='Password'/>
+                    <img className='registerIcon viewIcon' src={view} alt="eyeView Icon"/>
+                    <p className="passwordMessage"></p>
+                </div>
+
+                <button type="submit" className='submitBtn'>Login</button> 
+                <p className="submitMessage"></p>
+
+                <div className="goSignUpPage">
+                    <h2 className='goMessage'>Don't have an account?</h2>
+                    <button className="goRegister" onClick={() => navigate('/SignUp')}>Sign Up</button>
+                </div>
+            </form>
         </>
     )
 }
--- HabbitFlow/src/SignUpComponent/SignUpForm.jsx
+++ HabbitFlow/src/SignUpComponent/SignUpForm.jsx
@@ -1,14 +1,45 @@
 import { useNavigate } from 'react-router-dom'
+import view from '../assets/view.png'
+// import hide from '../assets/hide.png'
 
 const SignUpForm = () => {
     const navigate = useNavigate();
     return (
         <>
             <button className='goHomeBtn' onClick={() => navigate('/')}>Go Back to Homepage</button>
-        
-            <div className='signUpDiv'>
+            <form className='registerForm'>
+                <h1 className="registerTitle">Sign Up</h1>
+
+                <div className="usernameContainer">
+                    <input type="text" name="username" className='registerInput' id="username" placeholder='Username'/>
+                    <p className="userNameMessage"></p>
+                </div>
                 
-            </div>
+                <div className="emailContainer">
+                    <input type="text" name="email" className='registerInput' id="email" placeholder='Email'/>
+                    <p className="emailMessage"></p>
+                </div>
+
+                <div className="passwordContainer">
+                    <input type="password" name="password" className='registerInput' id="password" placeholder='Password'/>
+                    <img className='registerIcon viewIcon' src={view} alt="eyeView Icon"/>
+                    <p className="passwordMessage"></p>
+                </div>
+
+                <div className="passwordContainer">
+                    <input type="password" name="password2" className='registerInput' id="password2" placeholder='Re-Password'/>
+                    <img className='registerIcon viewIcon' src={view} alt="eyeView Icon"/>
+                    <p className="rePasswordMessage"></p>
+                </div>
+
+                <button type="submit" className='submitBtn'>Login</button> 
+                <p className="submitMessage"></p>
+
+                <div className="goSignUpPage">
+                    <h2 className='goMessage'>Already have an account?</h2>
+                    <button className="goRegister" onClick={() => navigate('/Login')}>Login</button>
+                </div>
+            </form>
         </>
     )
 }
