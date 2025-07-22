467e71a158cf0a5d99d687513695c2c7a6331552
Yasmine-Hmida <hmidayasmiine@gmail.com>
2025-07-22 16:51:24 +0100

    Complete all the functionalities of Habbits

--- HabbitFlow/src/Components/Habbit.jsx
+++ HabbitFlow/src/Components/Habbit.jsx
@@ -1,10 +1,27 @@
 import './Habbit.css'
-import bin from '../assets/bin.png'
 
-const Habbit = ({ habbitText, onDelete ,onClick}) => {
+import bin from '../assets/bin.png'
+import tick from '../assets/tick.png'
+import notTick from '../assets/not-tick.png'
+
+import { useState } from 'react';
+
+const Habbit = ({ habbitText, onDelete , id, isComplete , toggleCompleted}) => {
+    const [selected, setSelected] = useState(isComplete); 
+
+    const handleClick = () => {
+        setSelected((prev) => !prev);
+        toggleCompleted(id); // Update the global state and localStorage
+    };
+
     return (
-        <div className='HabbitContainer' onClick={onClick}>
-            <h1 className="habbitMessage">{habbitText}</h1>
+        <div className={`HabbitContainer ${selected ? 'selected' : ''}`}  onClick={handleClick}>
+
+            <div className="habbit">
+                <img className='checkDoneIcon' src={isComplete?tick:notTick} alt="Done Icon"/>
+                <h1 className="habbitMessage">{habbitText}</h1>
+            </div>
+
             <img 
                 src={bin} 
                 alt="Bin Icon" 
--- HabbitFlow/src/Pages/Habbits.jsx
+++ HabbitFlow/src/Pages/Habbits.jsx
@@ -5,6 +5,7 @@ import NavHabbits from '../Components/NavHabbits.jsx';
 import AddHabbitInput from '../Components/AddHabbitInput.jsx';
 import Habbit from '../Components/Habbit.jsx';
 
+import noResults from '../assets/no-results.png'
 import './Habbits.css';
 
 const Habbits = () => {
@@ -24,7 +25,14 @@ const Habbits = () => {
 
     // Add a new Habbit
     const handleAddHabbit = (newHabit) => {
-        const updatedHabbits = [...userHabbits, newHabit];
+        const newHabitItem = {
+            id: Date.now(),
+            text: newHabit,
+            isComplete: false
+        }
+
+        const updatedHabbits = [...userHabbits, newHabitItem];
+        console.log(updatedHabbits)
 
         // Save in state
         setUserHabbits(updatedHabbits);
@@ -48,8 +56,82 @@ const Habbits = () => {
         localStorage.setItem("userHabits", JSON.stringify(allHabbits));
     };
 
+    // Toggle Completed Task
+    const toggleCompleted = (id) => {
+        setUserHabbits((prevUserHabbits) => {
+            const updatedHabbits = prevUserHabbits.map((habbitItem) => {
+                if (habbitItem.id === id) {
+                    return { ...habbitItem, isComplete: !habbitItem.isComplete };
+                }
+                return habbitItem;
+            });
 
-    
+            // Save to localStorage
+            const allHabbits = JSON.parse(localStorage.getItem("userHabits")) || {};
+            allHabbits[username] = updatedHabbits;
+            localStorage.setItem("userHabits", JSON.stringify(allHabbits));
+
+            return updatedHabbits; // Return updated state
+        });
+    };
+
+    // Count the Completed Tasks
+    const countCompletedTasks = () => {
+        const allHabbits = JSON.parse(localStorage.getItem("userHabits")) || {};
+        const userHabbits = allHabbits[username];
+        let count = 0;
+
+        userHabbits.map((habbitItem) => {
+            if(habbitItem.isComplete){
+                count ++;
+            }
+        })
+        console.log(count)
+        return count
+    }
+
+    // Show the UserHabbits
+    const showUserHabbits = () => {
+        let isEmpty = false;
+
+        const allHabbits = JSON.parse(localStorage.getItem("userHabits")) || {};
+        const userHabbits = allHabbits[username];
+
+        if (!userHabbits || userHabbits.length === 0){
+            isEmpty = true
+        }
+
+        if(isEmpty){
+            return <div className='noResultsContainer'>
+                        <img src={noResults} alt='no Habbits Icon' className='noResult'/>
+                        <h2 className="noResultsMessage">No Habbits</h2>
+                   </div>
+        }
+        else{
+            return userHabbits.map((habbit, index) => (
+                    <Habbit
+                        key={index}
+                        id={habbit.id} 
+                        habbitText={habbit.text} 
+                        onDelete={() => handleDeleteHabbit(index)}
+                        isComplete = {habbit.isComplete}
+                        toggleCompleted={toggleCompleted}
+                    />
+            ))
+        }
+    }
+
+    // All Completed Tasks
+    const allCompleted = () => {
+        const totalCount = countCompletedTasks()
+        if(totalCount === userHabbits.length && userHabbits.length!=0){
+            return <h2 className='habbitCounter'>Good job, all Habbits Completed!</h2>
+        }
+        else{
+            return <h2 className='habbitCounter'>Keep Going: {countCompletedTasks()} / {userHabbits.length}</h2>
+        }
+    }
+
     return (
         <>
             <NavHabbits username={username} />
@@ -58,16 +140,10 @@ const Habbits = () => {
                 <AddHabbitInput onAdd={handleAddHabbit} />
             </div>
 
-            <h2 className='habbitCounter'>Keep Going: 0 / {userHabbits.length}</h2>
+            {allCompleted()}
 
             <div className="habbitsBox">
-                {userHabbits.map((habbit, index) => (
-                    <Habbit
-                        key={index} 
-                        habbitText={habbit} 
-                        onDelete={() => handleDeleteHabbit(index)}
-                    />
-                ))}
+                {showUserHabbits()}
             </div>
         </>
     );
